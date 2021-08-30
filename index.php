760d1b7605a43f89c6812b02f6ed0879484572c1
Alex00Sam <alsa00@mail.ru>
2021-08-30 20:33:03 +0300

    upd

--- index.php
+++ index.php
@@ -30,11 +30,11 @@ $col1 = $columns->addColumn(4);
 $col2 = $columns->addColumn(4);
 $col3 = $columns->addColumn(4);
 $rat->on('click',function($b)use($columns,$slot){
-  $slot = $slot->setOrder('total_rating');
+  $slot->setOrder('total_rating');
   return [$columns->jsReload()];
 });
 $dat->on('click',function($b)use($columns,$slot){
-  $slot = $slot->setOrder('date');
+  $slot->setOrder('date');
   return [$columns->jsReload()];
 });
 
