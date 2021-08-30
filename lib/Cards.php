b2683b46e33bd610cfc776f67902751cb7ef5f10
Alex00Sam <alsa00@mail.ru>
2021-08-30 23:20:48 +0300

    test

--- lib/Cards.php
+++ lib/Cards.php
@@ -17,16 +17,18 @@ class Cards extends \atk4\ui\View {
         $this->add(['Header',$obj['name']]);
     //    $this->add($rating);
     //    unset($rating);
-        $this->link(['party','id'=>$obj->id]);
+
         $rating = new \atk4\ui\View(['ui' => 'rating disabled']);
         $rating->id = '_rating_'.$obj->id;
         if($obj['total_rating']){
+          $this->link(['party','id'=>$obj->id]);
           $rating->js(true)->rating(['maxRating' => 5, 'initialRating' => round($obj['total_rating'])]);//->clear();
+          $this->add(['Label',$obj['total'],'icon'=>'users']);
         }else{
           $rating->js(true)->rating(['maxRating' => 5, 'initialRating' => round($obj['rating'])]);//->clear();
         }
         $this->add($rating);
-        $this->add(['Label',$obj['total'],'icon'=>'users']);
+
     //    $this->js()->clear();
       //  unset($this->_js_actions);
     }
