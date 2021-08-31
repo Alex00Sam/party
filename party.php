ec10e4fbbdfc561af55f600fb6d9c2c45a948b63
Alex00Sam <alsa00@mail.ru>
2021-08-31 22:53:16 +0300

    fix

--- party.php
+++ party.php
@@ -114,7 +114,7 @@
       $ch = $app->add(['Button','Редактировать слот']);
       $ch->on('click',new \atk4\ui\jsModal('Редактировать слот', $vp));
     }
-    $col2->add(['Label','Место:',['icon'=>'map marker alternate']]);
+    $col2->add(['Label','Место:',['icon'=>'map marker']]);
     $col2->add(['Header',$slot['place']]);
     if($slot['showmap']) {
       $map = new \atk4\ui\View(['template' => new \atk4\ui\Template('    <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q='.$slot['place'].'&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {height:400px!important;}</style></div>')]);
