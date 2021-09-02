ef41c9e10dac151392276d2acde36e7dccfc0337
Alex00Sam <alsa00@mail.ru>
2021-09-03 01:44:08 +0300

    test

--- lib/MLayout.php
+++ lib/MLayout.php
@@ -29,7 +29,8 @@ class MLayout extends \atk4\ui\Layout\Maestro
                 (new jQuery('body'))->toggleClass('atk-sidenav-visible'),
             ]);
             Icon::addTo($this->burger, ['content']);
-            Header::addTo($this->menu, [$this->app->title, 'size' => 4])->link(['index']);
+            //Header::addTo($this->menu, [$this->app->title, 'size' => 4])->link(['index']);
+            $this->menu->addItem('Vpiski.NET',['index']);
 
         }
 
