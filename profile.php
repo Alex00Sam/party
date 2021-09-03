960892d2845fc7133a0897df766d45bbbc0cb82b
Alex00Sam <alsa00@mail.ru>
2021-09-03 19:30:43 +0300

    test

--- profile.php
+++ profile.php
@@ -13,27 +13,27 @@
   $s2 = $card->addSection('О себе',$viewuser,['dob','description','vk','inst',]);
   $s2->addClass('center aligned');
   $s2 = $card->addSection('Контакты',$viewuser,['email','phone','country','city']);
+$vp = $app->add('VirtualPage');
+$vp->set(function ($vp) use ($current_user) {
+    $form = $vp->add('Form');
+    $form->setModel(new Users($db), ['login', 'password', 'name', 'surname', 'dob', 'image', 'email', 'phone', 'country', 'city', 'gender', 'description', 'vk', 'inst']);
+    $form->buttonSave->set('Сохранить');
+    $form->onSubmit(function ($f) {
+        $f->model->save();
 
+        return new \atk4\ui\jsExpression('document.location=""');
+    });
+
+});
   if($_SESSION['user_id']==$viewuser->id) {
-      $vp = $app->add('VirtualPage');
-      $vp->set(function ($vp) use ($current_user) {
-          $form = $vp->add('Form');
-          $form->setModel(new Users($db), ['login', 'password', 'name', 'surname', 'dob', 'image', 'email', 'phone', 'country', 'city', 'gender', 'description', 'vk', 'inst']);
-          $form->buttonSave->set('Сохранить');
-          $form->onSubmit(function ($f) {
-              $f->model->save();
 
-              return new \atk4\ui\jsExpression('document.location=""');
-          });
-
-      });
      // $mod = new \atk4\ui\jsModal('', $vp);
       //$mod->init();
      // $edit = new \atk4\ui\Button('Изменить');
      // $edit->init();
      // $app->add($edit);
       $edit = $app->add(['Button','test']);
-      $edit->on('click', $vp);
+      $edit->on('click',new \atk4\ui\jsModal( $vp));
 
    //   $card->addClickAction($vp,new \atk4\ui\Button('Изменить'));
       //
