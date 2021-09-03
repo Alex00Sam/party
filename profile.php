10be032df151c6b49464f3e73b35f3eb5ae9eab1
Alex00Sam <alsa00@mail.ru>
2021-09-03 18:38:57 +0300

    test

--- profile.php
+++ profile.php
@@ -27,12 +27,13 @@
           });
 
       });
-      $mod = $card->add(['jsModal','', $vp]);
+      $mod = new \atk4\ui\jsModal('', $vp);
+      $mod->init();
       $edit = new \atk4\ui\Button('Изменить');
       $edit->init();
     //  $edit->on('click', $mod);
       //$app->add($edit);
-      $card->addButton($edit)->on('click',$vp);
+      $card->addButton($edit)->on('click',$mod);
       //
 
   }
