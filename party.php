9fed5cd4e12a3ccfd7df3c92f173f04bf759d438
Alex00Sam <alsa00@mail.ru>
2021-09-03 14:40:23 +0300

    test

--- party.php
+++ party.php
@@ -123,8 +123,7 @@
       $col2->add($map);
     }
     $col2->add(['Label','Дата:','icon'=>'calendar alternate outline']);
-    $date = new \atk4\data\Field();
-    $date = $date->toString($slot->get('date'));
+    $date = (new \atk4\data\Field())->toString($slot->get('date'));
     $col2->add(['Header',$date]);
 
   //  $col2->add(['Label','test','image'=>$slot['image']]);
