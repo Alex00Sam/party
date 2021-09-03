17437f093794bd9359ce459f788eb42f189d488f
Alex00Sam <alsa00@mail.ru>
2021-09-03 22:45:59 +0300

    test

--- party.php
+++ party.php
@@ -57,11 +57,8 @@ $app->add($columns);
       }
     });
   $join=$app->add(['Button','Вступить'])->addClass('blue');
-if($slot['capacity'])
-    if($slot['capacity']<=$slot['total'] and !$mid->tryLoadBy('users_id',$_SESSION['user_id'])){
-        $join->addClass('basic disabled');
-        $join->set('Свободных мест нет');
-    }
+
+
     if(!isset($_SESSION['user_id'])){
       $join->addClass('basic disabled');
       $join->set('Войдите или зарегистрируйтесь, чтобы вступить');
@@ -95,6 +92,10 @@ if($slot['capacity'])
       }
 }
 
+if($slot['capacity']<=$slot['total'] and !$mid->tryLoadBy('users_id',$_SESSION['user_id'])){
+    $join->addClass('basic disabled');
+    $join->set('Свободных мест нет');
+}
 
     if($slot['creator_id']==$_SESSION['user_id']){
       $join->addClass('disabled');
