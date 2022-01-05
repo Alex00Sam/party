ba33f7e16ff90708dc933540a0dd3486b368f3e1
Alex00Sam <alsa00@mail.ru>
2022-01-05 23:11:54 +0200

    test

--- lib/Qty.php
+++ lib/Qty.php
@@ -4,7 +4,7 @@ class Qty extends \atk4\data\Model {
 	public $table = 'qty';
 	function init() :void{
 		parent::init();
-		$this->addField('qty',['type'=>'integer']);
+		$this->addField('qty',['type'=>'integer','default' => 1]);
 		
 	}
 }
